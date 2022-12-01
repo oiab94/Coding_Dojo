529ec9e87ff8776054c1327c4bed686958125578
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2022-12-01 17:43:48 -0300

    [JavaScript] Prueba de elementos onclick

---
+++ Fundamentos-de-la-Web/JS/elementoThis/script.js
@@ -0,0 +1,14 @@
+// Prueba de elemento onclick
+function ejemplo(elemento) {
+	console.log("elemento clickeado", elemento);
+}
+
+// Prueba de cambio de texto
+function turnOff(elemento) {
+	elemento.innerText = "Off";
+}
+
+// Prueba de remocion de un elemento
+function hide(elemento) {
+	elemento.remove();
+}
