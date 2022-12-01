eadb3e9bc6e6ad51e0b83e16f22fd42574dca4be
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2022-12-01 19:49:45 -0300

    [JavaScript] Tarea: Evento onclick

---
+++ Fundamentos-de-la-Web/JS/eventoOnClick/script.js
@@ -0,0 +1,14 @@
+// Prueba de elemento onclick
+function buttonLike(elemento) {
+	alert("Ninja was liked")
+}
+
+// Botón de login
+function buttonLogin(elemento) {
+	elemento.innerText = "Logout"
+}
+
+// Remover el boton Add Definition
+function removeButton(elemento) {
+	elemento.remove();
+}
